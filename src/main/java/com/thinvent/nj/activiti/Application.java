621f1d4daafacc39ca7c095f7cfbba2ab30629aa
liupj <liupj@nj-strongit.com.cn>
2018-12-11 17:35:59 +0800

    123

--- src/main/java/com/thinvent/nj/activiti/Application.java
+++ src/main/java/com/thinvent/nj/activiti/Application.java
@@ -20,7 +20,7 @@ import java.io.IOException;
 })
 @ComponentScan("com.thinvent.nj")
 public class Application {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 }
